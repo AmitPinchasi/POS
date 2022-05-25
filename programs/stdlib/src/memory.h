8555a2ea6e6cde6618f3bc4818e0067ad67af146
amitpinchasi <amitpinchasi123@gmail.com>
2022-05-25 18:58:18 +0300

    pushing

--- programs/stdlib/src/memory.h
+++ programs/stdlib/src/memory.h
@@ -1,10 +1,12 @@
 #ifndef POS_MEMORY_H
 #define POS_MEMORY_H
 
-#define SizeOf(type) (char *)(&type+1)-(char*)(&type)
+
 
 #include <stddef.h>
 void* memset(void* ptr, int c, size_t size);
 int memcmp(void* s1, void* s2, int count);
 void* memcpy(void* dest, void* src, int len);
+void* memmove(void *dest, void *src, size_t n)
+
 #endif
