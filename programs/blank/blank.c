821cc91f1289299a324e8d46ff2e71d6ff544c2b
AmitPinchasi <amitpinchasi123@gmail.com>
2022-05-25 12:58:12 -0400

    testing

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -2,8 +2,19 @@
 #include "stdlib.h"
 #include "stdio.h"
 #include "string.h"
+
 int main(int argc, char** argv)
 {
-    print("Hello, world!\n");
+    print("Staring Program..\n");
+    char* ptr= malloc(sizeof(char) * 10);
+    strcpy(ptr, "Hello World");
+    printf("%s\n", ptr);
+    char buf[1024];
+    pos_terminal_readline(buf, sizeof(buf), true);
+    print("\n");
+    printf(buf);
+
+    free(ptr);
+
     return 0;
 }
