bee1aa73701821c0ac221717462a079726aea4ee
amitpinchasi <amitpinchasi123@gmail.com>
2022-06-01 17:35:15 +0300

    stdint.h

---
+++ programs/stdlib/src/stdint.h
@@ -0,0 +1,39 @@
+// stdint.h implements the C99 standard integer types.
+//
+// The following standard integer types are defined:
+
+typedef signed char int8_t;
+typedef short int int16_t;
+typedef int int32_t;
+typedef long long int int64_t;
+
+typedef unsigned char uint8_t;
+typedef unsigned short int uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long int uint64_t;
+
+typedef signed char int_least8_t;
+typedef short int int_least16_t;
+typedef int int_least32_t;
+typedef long long int int_least64_t;
+
+typedef unsigned char uint_least8_t;
+typedef unsigned short int uint_least16_t;
+typedef unsigned int uint_least32_t;
+typedef unsigned long long int uint_least64_t;
+
+typedef signed char int_fast8_t;
+typedef short int int_fast16_t;
+typedef int int_fast32_t;
+typedef long long int int_fast64_t;
+
+typedef unsigned char uint_fast8_t;
+typedef unsigned short int uint_fast16_t;
+typedef unsigned int uint_fast32_t;
+typedef unsigned long long int uint_fast64_t;
+
+typedef long long int intmax_t;
+typedef unsigned long long int uintmax_t;
+
+typedef long long int intptr_t;
+typedef unsigned long long int uintptr_t;
